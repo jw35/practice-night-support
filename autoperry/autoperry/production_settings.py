2e9de92f472da301519d61de69563a604e026200
Jon Warbrick <jon.warbrick@googlemail.com>
2023-01-04 17:19:38 +0000

    Remove variant LOGING stansa in production_settings
    Was added with things at DEBUG sometime in the past.

--- autoperry/autoperry/production_settings.py
+++ autoperry/autoperry/production_settings.py
@@ -27,27 +27,3 @@ port: '587'
 username: 'autoperry@cambridgeringing.info'
 password: EMAIL_PASSWORD
 use_tls: True
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-        'django.server': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }}
