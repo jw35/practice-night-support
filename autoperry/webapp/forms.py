fc66f06880ea3817ce9bab34062853d30889fd52
Jon Warbrick <jon.warbrick@googlemail.com>
2023-01-09 17:53:34 +0000

    Re-layout Event creation form

--- autoperry/webapp/forms.py
+++ autoperry/webapp/forms.py
@@ -6,6 +6,9 @@ from django import forms
 from django.utils import timezone
 from django.core.exceptions import ValidationError
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
+
 from datetime import date, time, datetime
 
 class CustomUserCreationForm(UserCreationForm):
@@ -16,13 +19,35 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class EventForm(forms.Form):
-    date = forms.DateField(help_text='Event date', initial=date.today(), widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+    date = forms.DateField(help_text='Event date', initial=date.today, widget=forms.widgets.DateInput(attrs={'type': 'date'}))
     start_time = forms.TimeField(help_text='Event start time', initial="19:30", widget=forms.widgets.DateInput(attrs={'type': 'time', 'step': 300}))
     end_time = forms.TimeField(help_text='Event finish time', initial="21:00", widget=forms.widgets.DateInput(attrs={'type': 'time', 'step': 300}))
     location = forms.CharField(max_length=60, help_text='Where the event takes place')
     helpers_required = forms.IntegerField(min_value=1, help_text='Number of helpers wanted', initial=1)
     contact_address = forms.CharField(max_length=60, required=False, help_text="Email address for the event (yours if blank)")
     notes = forms.CharField(max_length=128, required=False, help_text="Purpose of the event, helper skills required, etc. (optional)")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'event-create'
+        self.helper.layout = Layout(
+            Row(
+                Column('date', css_class='form-group col-md-4 mb-0'),
+                Column('start_time', css_class='form-group col-md-4 mb-0'),
+                Column('end_time', css_class='form-group col-md-4 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('location', css_class='form-group col-md-8 mb-0'),
+                Column('helpers_required', css_class='form-group col-md-4 mb-0'),
+                css_class='form-row'
+            ),
+            'contact_address',
+            'notes',
+            Submit('submit', 'Create')
+        )
 
     def clean(self):
         cleaned_data = super().clean()
