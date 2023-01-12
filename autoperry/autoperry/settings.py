84b2c19d669fbbe2b41496beecc69e4b426b9d19
Jon Warbrick <jon.warbrick@googlemail.com>
2023-01-12 13:45:27 +0000

    Turn debugging back on in settings (but not production-settings!)

--- autoperry/autoperry/settings.py
+++ autoperry/autoperry/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x(6$#&$7wc4+^kgk+&1ni9!#xb(pe0lo1)5n8i47rtn0jigr1b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
