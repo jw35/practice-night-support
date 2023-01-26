396dff0d770806cb0e63cfbdf88ceb6856e4178a
Jon Warbrick <jon.warbrick@googlemail.com>
2023-01-26 13:43:23 +0000

    Add the address to the email log message

--- autoperry/webapp/util.py
+++ autoperry/webapp/util.py
@@ -12,18 +12,19 @@ logger = logging.getLogger(__name__)
 
 def send_template_email(to,template,context):
 
-	"""
-	Construct an email from the supplied template path and context
-	and send it to the user
-	"""
+    """
+    Construct an email from the supplied template path and context
+    and send it to the user
+    """
 
-	message = render_to_string(f"webapp/email/{template}-message.txt", context).strip()
-	subject = render_to_string(f"webapp/email/{template}-subject.txt", context).strip()
-	if isinstance(to, User):
-		to.email_user(subject, message)
-	else:
-		send_mail(subject, message, None, [to])
-	logger.info(f'Emailed {to} "{subject}"')
+    message = render_to_string(f"webapp/email/{template}-message.txt", context).strip()
+    subject = render_to_string(f"webapp/email/{template}-subject.txt", context).strip()
+    if isinstance(to, User):
+        to.email_user(subject, message)
+        logger.info(f'Emailed user {to} {to.email} "{subject}"')
+    else:
+        send_mail(subject, message, None, [to])
+        logger.info(f'Emailed address {to} "{subject}"')
 
 def login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
 
