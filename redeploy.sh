f45822d2c4e78f3e4ce84eb9d149cb9f1702bb63
Jon Warbrick <jon.warbrick@googlemail.com>
2023-02-10 10:57:20 +0000

    Add a 'redeploy' script

---
+++ redeploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+export DJANGO_SETTINGS_MODULE=autoperry.production_settings
+
+. venv/bin/activate
+
+pip install -r production-requirements.txt
+
+git pull
+
+cd autoperry
+
+./manage.py collectstatic --no-input
+./manage.py migrate
+
+systemctl --user restart autoperry
+
