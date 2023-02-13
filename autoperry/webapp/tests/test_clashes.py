3018e349d04af6bda27b3d1da37f0d1c0c6b2759
Jon Warbrick <jon.warbrick@googlemail.com>
2023-02-13 16:59:22 +0000

    Refactor clash tests (loops ot cut-and-past fn calls)

--- autoperry/webapp/tests/test_clashes.py
+++ autoperry/webapp/tests/test_clashes.py
@@ -42,28 +42,43 @@ class ClashesTestCase(TestCase):
         end = start + timedelta(hours=2)
 
 
-        well_before = start - timedelta(hours=2)
-        before = start - timedelta(hours=1)
+        before = start - timedelta(hours=2)
+        just_before = start - timedelta(hours=1)
         just_started = start + timedelta(minutes=5)
-        middle = start + timedelta(hours=1)
         almost_finished = end - timedelta(minutes=5)
-        after = end+timedelta(hours=1)
-        well_after = end+timedelta(hours=2)
+        just_after = end+timedelta(hours=1)
+        after = end+timedelta(hours=2)
 
         cls.event_spec = {
-            'before':      [well_before,     before],
+            'before':      [before,          just_before],
             'upto_start':  [before,          start],
-            'into':        [before,          middle],
+            'into':        [before,          just_started],
             'upto_end':    [before,          end],
             'contains':    [before,          after],
-            'first_part':  [start,           middle],
+            'first_part':  [start,           just_started],
             'all':         [start,           end],
             'from_start':  [start,           after],
             'within':      [just_started,    almost_finished],
-            'last_part':   [middle,          end],
-            'out_of':      [middle,          after],
+            'last_part':   [almost_finished, end],
+            'out_of':      [almost_finished, after],
             'from_end':    [end,             after],
-            'after':       [after,           well_after],
+            'after':       [just_after,      after],
+        }
+
+        cls.event_expected = {
+            'before':      False,
+            'upto_start':  False,
+            'into':        True,
+            'upto_end':    True,
+            'contains':    True,
+            'first_part':  True,
+            'all':         True,
+            'from_start':  True,
+            'within':      True,
+            'last_part':   True,
+            'out_of':      True,
+            'from_end':    False,
+            'after':       False,
         }
 
         cls.location = 'Little Shelford'
@@ -82,82 +97,35 @@ class ClashesTestCase(TestCase):
         cls.event.helpers.add(cls.live)
 
 
-    def test_event_shouldnt_clash(self):
+    def test_event(self):
 
-        self.assertFalse(event_clash_error(*self.event_spec['before'], self.location))
-        self.assertFalse(event_clash_error(*self.event_spec['upto_start'], self.location))
-        self.assertFalse(event_clash_error(*self.event_spec['from_end'], self.location))
-        self.assertFalse(event_clash_error(*self.event_spec['after'], self.location))
+        for key in self.event_spec.keys():
+            with self.subTest(key):
+                self.assertEqual(bool(event_clash_error(*self.event_spec[key], self.location)), self.event_expected[key])
 
-    def test_event_should_clash(self):
-
-        self.assertTrue(event_clash_error(*self.event_spec['into'], self.location))
-        self.assertTrue(event_clash_error(*self.event_spec['upto_end'], self.location))
-        self.assertTrue(event_clash_error(*self.event_spec['contains'], self.location))
-
-        self.assertTrue(event_clash_error(*self.event_spec['first_part'], self.location))
-        self.assertTrue(event_clash_error(*self.event_spec['all'], self.location))
-        self.assertTrue(event_clash_error(*self.event_spec['from_start'], self.location))
-
-        self.assertTrue(event_clash_error(*self.event_spec['within'], self.location))
-        self.assertTrue(event_clash_error(*self.event_spec['last_part'], self.location))
-
-        self.assertTrue(event_clash_error(*self.event_spec['out_of'], self.location))
 
     def test_event_elsewhere(self):
 
-        self.assertFalse(event_clash_error(*self.event_spec['before'], self.elsewhere))
-
-        self.assertFalse(event_clash_error(*self.event_spec['upto_start'], self.elsewhere))
-        self.assertFalse(event_clash_error(*self.event_spec['into'], self.elsewhere))
-        self.assertFalse(event_clash_error(*self.event_spec['upto_end'], self.elsewhere))
-        self.assertFalse(event_clash_error(*self.event_spec['contains'], self.elsewhere))
-
-        self.assertFalse(event_clash_error(*self.event_spec['first_part'], self.elsewhere))
-        self.assertFalse(event_clash_error(*self.event_spec['all'], self.elsewhere))
-        self.assertFalse(event_clash_error(*self.event_spec['from_start'], self.elsewhere))
-
-        self.assertFalse(event_clash_error(*self.event_spec['within'], self.elsewhere))
-        self.assertFalse(event_clash_error(*self.event_spec['last_part'], self.elsewhere))
-
-        self.assertFalse(event_clash_error(*self.event_spec['out_of'], self.elsewhere))
-
-        self.assertFalse(event_clash_error(*self.event_spec['from_end'], self.elsewhere))
-
-        self.assertFalse(event_clash_error(*self.event_spec['after'], self.elsewhere))
+        for key in self.event_spec.keys():
+            with self.subTest(key):
+                self.assertEqual(bool(event_clash_error(*self.event_spec[key], self.elsewhere)), False)
 
 
     def test_volounteering(self):
 
-        # Make some test events
-
-        test_event = {}
         for key in self.event_spec.keys():
-            test_event[key] = Event.objects.create(
-                start=self.event_spec[key][0],
-                end=self.event_spec[key][1],
-                location=self.location,
-                helpers_required=2,
-                owner=self.live,
-                contact_address=None,
-                notes=key,
-                alerts=True)
+            with self.subTest(key):
+                event = Event.objects.create(
+                    start=self.event_spec[key][0],
+                    end=self.event_spec[key][1],
+                    location=self.location,
+                    helpers_required=2,
+                    owner=self.live,
+                    contact_address=None,
+                    notes=key,
+                    alerts=True)
 
-        self.assertFalse(volunteer_clash_error(self.live, test_event['before']))
-        self.assertFalse(volunteer_clash_error(self.live, test_event['upto_start']))
-        self.assertFalse(volunteer_clash_error(self.live, test_event['from_end']))
-        self.assertFalse(volunteer_clash_error(self.live, test_event['after']))
-
-        self.assertTrue(volunteer_clash_error(self.live, test_event['into']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['upto_end']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['contains']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['first_part']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['all']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['from_start']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['within']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['last_part']))
-        self.assertTrue(volunteer_clash_error(self.live, test_event['out_of']))
-
+                self.assertEqual(bool(volunteer_clash_error(self.live, event)), self.event_expected[key])
 
 
 
