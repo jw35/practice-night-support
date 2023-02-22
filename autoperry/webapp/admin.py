e91319498228037b505652017d656ea5e8fe303e
Jon Warbrick <jon.warbrick@googlemail.com>
2023-02-22 13:42:52 +0000

    Add 'created', 'past' and 'helpers_needed' to Django admio display

--- autoperry/webapp/admin.py
+++ autoperry/webapp/admin.py
@@ -6,16 +6,20 @@ from webapp.models import Event, Volunteer
 
 class HelpersInline(admin.TabularInline):
     model = Event.helpers.through
+    readonly_fields = ['created', 'current']
     extra = 1
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     date_hierarchy = 'start'
-    search_fields = ['location', 'owner__first_name', 'owner__last_name', 'helpers__first_name', 'helpers__last_name']
+    search_fields = ['location', 'owner__first_name', 'owner__last_name',
+                     'helpers__first_name', 'helpers__last_name']
     inlines = [
         HelpersInline,
     ]
-    fields = ['start', 'end', 'location', 'helpers_required', 'owner', 'created', 'cancelled', 'contact_address', 'notes', 'owner_reminded']
-    readonly_fields = ['created']
+    fields = ['start', 'end', 'location', 'helpers_required', 'owner', 'created',
+              'cancelled', 'past', 'helpers_needed', 'contact_address', 'notes',
+              'owner_reminded']
+    readonly_fields = ['created', 'past', 'helpers_needed']
     view_on_site = True
 
